71110dabd910cc52d85603c2868f8d4a2c3a2fad
Alan Andrés Sánchez Castro <pony.alanandres@gmail.com>
2015-06-22 11:21:17 -0500

    Se creó el esqueleto de la aplicación (header-footer).

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!-- Start Footer -->
+      <footer>
+        <p>&copy; Company 2015</p>
+      </footer>
+    </div> <!-- /container -->        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="bower_components/jquery/dist/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
--- test.php
+++ test.php
@@ -1,6 +1,28 @@
 <?php
-	include_once 'functions.php';
-	print_r(BaseCtrl::getUser(9810,"testRoot"));
-	print_r(BaseCtrl::getUser(1000,"testGerente"));
-	print_r(BaseCtrl::getUser(1000,"testRoot"));
+	include_once 'header.php';
+?>
+
+<!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p><?php print_r(BaseCtrl::getUser(9810,"testRoot")); ?></p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p><?php print_r(BaseCtrl::getUser(1000,"testGerente")); ?></p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p><?php print_r(BaseCtrl::getUser(1000,"testRoot")); ?></p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+
+      <hr>
+
+<?php
+    include_once 'footer.php';
 ?>
