f2a927de84dd78da4752cf567f2ba027423240d2
Alan Andrés Sánchez Castro <pony.alanandres@gmail.com>
2015-07-30 12:00:41 -0500

    Se separó el javascript para mayor control.

---
+++ cuenta.js
@@ -0,0 +1,275 @@
+var comentando = null;
+
+$(function(){
+	$.each($('.src-list'), function(k,v){
+		Sortable.create(v, {
+			group: {
+				name: "Grupo1", 
+				pull: 'clone', 
+				put: false
+			},
+			sort: false
+		});
+	});
+
+	var crearLista = function(lista, grupo){
+		Sortable.create(lista, { 
+			group: {
+				name: grupo,
+				put: ["Grupo1", grupo]
+			},
+			sort: false,
+			onAdd: function(evt){
+				var itemEl = evt.item;	// dragged HTMLElement
+				var jEl = $(itemEl);
+				var subTotal = 0;
+				var totalContainer = null;
+				if($(evt.from).hasClass("simpleList")){
+					totalContainer = $(evt.from).parent().find(".total-cuenta");
+				 	subTotal = parseInt(totalContainer.find(".sub-total").attr('data-subtotal')) - parseInt(jEl.find('.precio').html());
+				 	
+				 	totalContainer.find(".sub-total").attr('data-subtotal', subTotal);
+				 	totalContainer.find(".sub-total").html(subTotal);
+
+				 	totalContainer.find(".total").attr('data-total', subTotal);
+				 	totalContainer.find(".total").html(subTotal);
+				}
+
+			 	var ul = $(itemEl).parent();
+			 	ul.find($(itemEl)).remove();
+			 	ul.append(itemEl);
+			 	ul.parent().find("[name=cuentaActivaRadio]").prop("checked", true).change();
+
+			 	totalContainer = ul.parent().find(".total-cuenta");
+			 	subTotal = parseInt(totalContainer.find(".sub-total").attr('data-subtotal')) + parseInt(jEl.find('.precio').html());
+			 	
+			 	totalContainer.find(".sub-total").attr('data-subtotal', subTotal);
+			 	totalContainer.find(".sub-total").html(subTotal);
+
+			 	totalContainer.find(".total").attr('data-total', subTotal);
+			 	totalContainer.find(".total").html(subTotal);
+
+			 	if(jEl.children().attr("data-pedido")==0){
+			 		jEl.find(".botones").removeClass("hide");
+			 	}
+			 	jEl.find('.btn-eliminar').on('click touchend', function(){
+			 		jEl.parent().parent().find("[name=cuentaActivaRadio]").prop("checked", true).change();
+
+			 		jEl.remove();
+			 		
+			 		var subTotal = parseInt(totalContainer.find(".sub-total").attr('data-subtotal')) - parseInt(jEl.find('.precio').html());
+			 	
+				 	totalContainer.find(".sub-total").attr('data-subtotal', subTotal);
+				 	totalContainer.find(".sub-total").html(subTotal);
+
+				 	totalContainer.find(".total").attr('data-total', subTotal);
+				 	totalContainer.find(".total").html(subTotal);
+			 	});
+			},
+			scroll: true
+		 });
+	};
+
+	$(document).on("click touchend", ".mesasActivasContainer > button", function(){
+		var nombre = $(this).html();
+		lista = $("[data-nombre="+nombre);
+		$(this).toggleClass("disabled");
+		if(lista.attr("data-active")=="1"){
+			lista.attr("data-active",0);
+			lista.find("[name=cuentaActivaRadio]").prop("checked", false);
+			if(lista.nextAll(":not(.hide)").length){
+				$(lista.nextAll(":not(.hide)")[0]).find("[name=cuentaActivaRadio]").prop("checked", true).change();
+			}else if(lista.prevAll(":not(.hide)").length){
+				$(lista.prevAll(":not(.hide)")[0]).find("[name=cuentaActivaRadio]").prop("checked", true).change();
+			}
+		}else if(lista.hasClass("hide")){
+			lista.find("[name=cuentaActivaRadio]").prop("checked", true).change();
+		}
+		lista.toggleClass("hide");
+	});
+
+	$.each($('.listas-secundarias:not(#mainList)'), function(k, v){
+		var nombre = $(v).attr("data-nombre");
+		crearLista($(v).find(".simpleList")[0], $(v).attr('data-grupo'));
+		var boton = $('<button type="button" class="btn btn-primary btn-lg" style="margin: 5px;" data-nombre-boton="'+nombre+'">'+nombre+'</button>');
+		$(".mesasActivasContainer").append(boton);
+		if($(v).attr('data-active')==0){
+			boton.trigger('click');
+		}
+	});
+
+	var clonarLista = function(nombre, grupo){
+		if(nombre.length != 0){
+			var otherList = $("#mainList").clone();
+			otherList.attr("data-nombre", nombre);
+			otherList.attr("data-grupo", grupo);
+			otherList.attr("data-iter", 0);
+			otherList.removeAttr("id");
+			otherList.find(".tituloCuenta").html(nombre);
+			otherList.removeClass("hide");
+			otherList.find("[name=cuentaActivaRadio]").prop("checked", true).change();
+			$("[data-active=1]").attr("data-active",0);
+			otherList.attr("data-active", "1");
+			crearLista(otherList.find(".simpleList")[0], grupo);
+			$(".mesasActivasContainer").append($('<button type="button" class="btn btn-primary btn-lg" style="margin: 5px;" data-nombre-boton="'+nombre+'">'+nombre+'</button>'));
+			return otherList;
+		}
+	}
+
+	$("#crearCuenta").submit(function(e){
+		e.preventDefault();
+		var nombre = $("#nombreNuevaCuenta").val();
+		
+		if(nombre && $("[data-nombre="+nombre+"]").length == 0 ){
+			var otherList = clonarLista(nombre, nombre);
+			$("#mainList").parent().append(otherList);
+			$("#alertNuevaCuenta").addClass("hide")
+			$(this).parent().modal('hide');
+		}else{
+			$("#alertNuevaCuenta").removeClass("hide")
+		}
+	});
+
+	$(".dividirCuenta").on("click touchend",function(){
+		var lista = $("[data-active=1]");
+		if(lista.length != 0){
+			lista.attr("data-iter",parseInt(lista.attr("data-iter"))+1);
+			var nombre = lista.attr("data-nombre")+'-'+lista.attr("data-iter");
+			var grupo = lista.attr("data-grupo");
+			var otherList = clonarLista(nombre, grupo);
+			lista.after(otherList);
+		}
+	});
+
+	$(".cobrarCuenta").on('click touchend', function(){
+		var tabla = $("#tabla-cuenta");
+		tabla.empty();
+		var ulActivo = $("#listasContainer>[data-active=1]>ul");
+		if(ulActivo.length!=0){
+			$(".bs-example-modal-sm").modal("show");
+			$.each(ulActivo.find("li"), function(k,v){
+				tabla.append("<tr><td>"+$(v).find(".nombre").html()+"</td><td>"+$(v).find(".precio").html()+"</td><td></td><td></td></tr>");
+			});
+			tabla.parent().find(".total").html(ulActivo.parent().find(".total").html());
+			$("#sobra").html(-ulActivo.parent().find(".total").html());
+			$("#sobra").addClass('text-danger');
+			$("#monto").val('');
+			$("#monto2").val('');
+			tabla.parent().find(".sub-total").html(ulActivo.parent().find(".sub-total").html());
+		}
+	});
+
+	$(".pedirComanda").on('click touchend', function(){
+		var ulActivo 		= $("#listasContainer>[data-active=1]>ul");
+		var parent 			= ulActivo.parent();
+		var nombreCuenta 	= parent.attr("data-nombre"),
+			grupo 			= parent.attr("data-grupo");
+			comentario 		= parent.attr("data-comentario").replace(/<br \/>/g, "\n");
+			id				= parent.attr("data-id");
+		if(ulActivo.length!=0){
+			var cuenta = {
+							nombre		: parent.attr("data-nombre"),
+							grupo		: parent.attr("data-grupo"),
+							comentario	: parent.attr("data-comentario").replace(/<br \/>/g, "\n"),
+							id			: parent.attr("data-id"),
+							productos	:[],
+						};
+			$.each(ulActivo.find("li"), function(k,v){
+				var producto = $(v).children();
+				if(producto.attr("data-pedido")==0){
+					$(v).find(".botones").addClass("hide");
+					producto.attr("data-pedido", 1);
+					cuenta.productos.push({
+						id: producto.attr("data-id"),
+						comentario: producto.attr("data-comentario").replace(/<br \/>/g, "\n"),
+						area: producto.attr("data-area")
+					});
+				}
+			});
+			if(cuenta.productos.length != 0){
+				$.ajax({
+					type: "POST",
+					dataType: "json",
+					url: "pedirComanda.php",
+					data: {
+						'cuenta':cuenta
+					},
+					success: function(data){
+						if(data.ok){
+					    	window.location.href = '?cuenta='+data.id;
+						}
+					},
+					error: function(e){
+					    console.log(e.message);
+					}
+				});
+			}
+		}
+	});
+
+	$("#monto, #monto2").on('input',function(){
+		var totalCuenta = parseInt($(this).parent().parent().parent().parent().parent().find(".total").html());
+		var monto1 = $("#monto").val() || 0,
+			monto2 = $("#monto2").val() || 0;
+
+		var sobra = -(totalCuenta-parseInt(monto1)-parseInt(monto2));
+
+		$("#sobra").html(sobra);
+
+		if(sobra<0){
+			$("#sobra").addClass('text-danger');
+		}else{
+			$("#sobra").removeClass('text-danger');
+		}
+	});
+
+	$('#modalNuevaCuenta').on('shown.bs.modal', function () {
+	    $("#nombreNuevaCuenta").focus();
+	});
+	$('#modalNuevaCuenta').on('hidden.bs.modal', function () {
+	    $("#nombreNuevaCuenta").val('');
+	});
+
+	$('#modalComentario').on('shown.bs.modal', function () {
+	    $("#comentario").focus();
+	});
+	$('#modalComentario').on('hidden.bs.modal', function () {
+	    $("#comentario").val('');
+	    comentando = null;
+	});
+
+	$(document).on("change", "[name=cuentaActivaRadio]", function(){
+		$("[data-active=1]").attr("data-active", "0");
+		$(this).parent().parent().parent().attr("data-active", "1");
+	});
+
+
+	$(document).on("click touchend", ".btn-comentario", function(){
+		var element = $(this);
+		comentando = element;
+		if(element.attr("data-cuenta") == 1){
+			var nombreCuenta = element.parent().parent().find('.tituloCuenta').html();
+			$("#labelComentario").html(nombreCuenta);
+		}else{
+			var nombreProducto = element.parent().parent().find('.nombre').html();
+			var nombreCuenta = element.closest('[data-nombre]').attr("data-nombre");
+			$("#labelComentario").html(nombreCuenta +" | "+ nombreProducto);
+		}
+		_el = element.parent().parent();
+		$("#comentario").val(element.parent().parent().attr("data-comentario").replace(/<br \/>/g, "\n"));
+	});
+
+	$("#ingresarComentario").submit(function(e){
+		e.preventDefault();
+		comentando.parent().parent().attr("data-comentario", $("#comentario").val());
+		if($("#comentario").val().length > 0){
+			comentando.removeClass("btn-info");
+			comentando.addClass("btn-success");
+		}else{
+			comentando.addClass("btn-info");
+			comentando.removeClass("btn-success");
+		}
+		$(this).parent().modal('hide');
+		
+	});
+});
